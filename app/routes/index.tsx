3f51b53a0d72506031b436ee06b79b1519cf5938
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2022-04-21 22:22:08 +0200

    Update react and fetch data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,25 @@
+import type { LoaderFunction } from "@remix-run/vercel" 
+import { useLoaderData } from "@remix-run/react"
+
+export const loader: LoaderFunction = async({}) => {
+  const baseAPI = "https://api.eloverblik.dk"
+  
+  const token = process.env.EL_TOKEN
+  const accessToken = await fetch(baseAPI + "​/api​/token", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  return accessToken
+}
+
 export default function Index() {
+  const data = useLoaderData()
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      {data}
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
