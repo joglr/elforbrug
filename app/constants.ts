6c048ba0d94bcc507adb29e1ff791552ca81d0be
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-08-24 14:44:38 +0200

    Add routing and period selection links

---
+++ app/constants.ts
@@ -0,0 +1,22 @@
+export const periods = {
+  "30D": {
+    label: "30D",
+    aggregation: "Day",
+    default: false,
+  },
+  "14D": {
+    label: "14D",
+    aggregation: "Day",
+    default: false,
+  },
+  "7D": {
+    label: "7D",
+    aggregation: "Day",
+    default: false,
+  },
+  "24H": {
+    label: "24H",
+    aggregation: "Hour",
+    default: true,
+  },
+};
